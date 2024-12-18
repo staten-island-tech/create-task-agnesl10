b639ade06c37034a92c99a3bc4635a418c6f897a
agnesl10 <agnesl10@nycstudents.net>
2024-12-18 12:32:28 -0500

    sdfsdf

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,9 +1,10 @@
 DOMSelectors = {
-    rows: document.querySelector(".row"),
-    input: document.querySelector(".user"),
-    game: document.querySelector(".game"),
-    container: document.querySelector(".container"),
-    button: document.querySelector(".btn"),
-    hintbtn: document.querySelector(".hint"),
-}
+  rows: document.querySelector(".row"),
+  tiles: document.querySelector(".tile"),
+  input: document.querySelector(".user"),
+  game: document.querySelector(".game"),
+  container: document.querySelector(".container"),
+  button: document.querySelector(".btn"),
+  hintbtn: document.querySelector(".hint"),
+};
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,3 +1,16 @@
 import "../CSS/style.css";
 import { randomWords } from "./words";
-import { DOMSelectors} from "./dom";
+import { DOMSelectors } from "./dom";
+
+function setColor(letter) {
+  //classes: correct, present, incorrect
+  DOMSelectors.tiles.classList.remove("correct", "present", "incorrect");
+  //set the classes
+  if (letter === "correct") {
+    DOMSelectors.tiles.classlist.add("correct"); //add correct == css color change; same for the others ..
+  } else if (letter === "present") {
+    DOMSelectors.tiles.classlist.add("present");
+  } else {
+    DOMSelectors.tiles.classlist.add("incorrect");
+  }
+}
