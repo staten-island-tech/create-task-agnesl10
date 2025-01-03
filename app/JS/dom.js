44f69473fcf80060d2bba8224dc0d59ef3f77a9d
agnesl10 <agnesl10@nycstudents.net>
2025-01-03 12:32:11 -0500

    sdfsdf

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,9 +1,14 @@
 DOMSelectors = {
   rows: document.querySelector(".row"),
+  row1: document.querySelector("#guess1"),
+  row2: document.querySelector("#guess2"),
+  row3: document.querySelector("#guess3"),
+  row4: document.querySelector("#guess4"),
+  row5: document.querySelector("#guess5"),
+  row6: document.querySelector("#guess6"),
   tiles: document.querySelector("#tile"),
   input: document.querySelector(".user"),
-  game: document.querySelector(".game"),
-  container: document.querySelector(".container"),
+  container: document.querySelector(".board"),
   button: document.querySelector(".btn"),
   hintbtn: document.querySelector(".hint"),
 };
--- app/JS/main.js
+++ app/JS/main.js
@@ -17,3 +17,27 @@ function setWord(letter) {
 
 const random = randomWords[randomIndex];
 console.log(random);
+
+rows.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value.length === input.maxLength) {
+      const nextInput = input[index + 1];
+      if (nextInput) {
+        nextInput.focus();
+      }
+    }
+  });
+});
+
+DOMSelectors.button.addEventListener("click", function (e) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="row" id="guess2" />
+    <input type="text" maxlength="1" class="" id="tile1" />
+    <input type="text" maxlength="1" class="" id="tile2" />
+    <input type="text" maxlength="1" class="" id="tile3" />
+    <input type="text" maxlength="1" class="" id="tile4" />
+    <input type="text" maxlength="1" class="" id="tile5" />
+    </div>`
+  );
+});
