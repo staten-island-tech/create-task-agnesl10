4962827abe9c51b017e10d7ef057e872c8b452dc
agnesl10 <agnesl10@nycstudents.net>
2025-01-06 12:34:02 -0500

    sdfsdf

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,15 +1,15 @@
-DOMSelectors = {
-  rows: document.querySelector(".row"),
-  row1: document.querySelector("#guess1"),
-  row2: document.querySelector("#guess2"),
-  row3: document.querySelector("#guess3"),
-  row4: document.querySelector("#guess4"),
-  row5: document.querySelector("#guess5"),
-  row6: document.querySelector("#guess6"),
+const DOMSelectors = {
+  rows: document.querySelectorAll(".row"),
+  // row1: document.querySelector("#guess1"),
+  // row2: document.querySelector("#guess2"),
+  // row3: document.querySelector("#guess3"),
+  // row4: document.querySelector("#guess4"),
+  // row5: document.querySelector("#guess5"),
+  // row6: document.querySelector("#guess6"),
   tiles: document.querySelector("#tile"),
   input: document.querySelector(".user"),
   container: document.querySelector(".board"),
-  button: document.querySelector(".btn"),
+  button: document.querySelector(".submit"),
   hintbtn: document.querySelector(".hint"),
 };
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -2,23 +2,32 @@ import "../CSS/style.css";
 import { randomWords } from "./words";
 import { DOMSelectors } from "./dom";
 
+const target = randomWords[Math.floor(Math.random() * randomWords.length)].word;
+console.log(target);
+
 function setWord(letter) {
-  //classes: correct, present, incorrect
-  DOMSelectors.tiles.classList.remove("correct", "present", "incorrect");
-  //set the classes
-  if (letter === "correct") {
-    DOMSelectors.tiles.classlist.add("correct"); //add correct == css color change; same for the others ..
-  } else if (letter === "present") {
-    DOMSelectors.tiles.classlist.add("present");
-  } else {
-    DOMSelectors.tiles.classlist.add("incorrect");
+  const targetWord = target.toUpperCase();
+
+  if ((DOMSelectors.input = "")) {
+    alert("there seems to be missing information. check it out.");
   }
+
+  let index = 0;
+  DOMSelectors.button.addEventListener("click", function (e) {
+    DOMSelectors.tiles.classList.remove("correct", "present", "incorrect");
+    //set the classes
+    if (letter === input[index]) {
+      DOMSelectors.tiles.classlist.add("correct"); //add correct == css color change; same for the others ..
+    } else if (targetWord.includes(letter[index])) {
+      DOMSelectors.tiles.classlist.add("present");
+    } else {
+      DOMSelectors.tiles.classlist.add("incorrect");
+    }
+    index = index + 1;
+  });
 }
-
-const random = randomWords[randomIndex];
-console.log(random);
-
-rows.forEach((input, index) => {
+setWord();
+DOMSelectors.rows.forEach((input, index) => {
   input.addEventListener("input", () => {
     if (input.value.length === input.maxLength) {
       const nextInput = input[index + 1];
@@ -32,7 +41,7 @@ rows.forEach((input, index) => {
 DOMSelectors.button.addEventListener("click", function (e) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
-    `<div class="row" id="guess2" />
+    `<div class="row" id="guess" />
     <input type="text" maxlength="1" class="" id="tile1" />
     <input type="text" maxlength="1" class="" id="tile2" />
     <input type="text" maxlength="1" class="" id="tile3" />
