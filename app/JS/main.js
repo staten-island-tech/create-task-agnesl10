42199de1836c8ea9db705285691979873f4004c9
agnesl10 <agnesl10@nycstudents.net>
2025-01-09 12:30:48 -0500

    sdfsdf

--- app/JS/main.js
+++ app/JS/main.js
@@ -9,6 +9,7 @@ import { DOMSelectors } from "./dom";
 const target = randomWords[Math.floor(Math.random() * randomWords.length)].word;
 console.log(target);
 let attempts = 0;
+let guess = "";
 
 function setWord() {
   const targetWord = target.toUpperCase();
@@ -22,6 +23,8 @@ function setWord() {
 }
 
 function addRow() {
+  checkGuess();
+
   if (attempts < 5) {
     attempts = attempts + 1;
     console.log(attempts);
@@ -40,16 +43,41 @@ function addRow() {
   }
 }
 
+function checkGuess() {
+  document
+    .querySelectorAll(".test")
+    .forEach((input) => (guess = guess + `${input.value}`));
+  console.log(guess);
+
+  let i = 0;
+  DOMSelectors.tiles.classList.remove("correct", "incorrect", "present");
+  for (guess[i] in guess) {
+    if (guess[i] === targetWord[i]) {
+      DOMSelectors.tiles.classList.add("correct");
+      clearGuess(guess);
+    } else if (targetWord.includes(guess[i])) {
+      DOMSelectors.tiles.classList.add("present");
+      clearGuess(guess);
+    } else {
+      DOMSelectors.tiles.classList.add("incorrect");
+      clearGuess(guess);
+    }
+  }
+}
+
+function clearGuess(guess) {
+  guess = "";
+}
+
 setWord();
 
-DOMSelectors.rows.forEach((tiles, index) => {
-  DOMSelectors.tiles.addEventListener("input", () => {
-    if (tiles.value.length === tiles.maxLength) {
-      const nextInput = tiles[index + 1];
-      if (nextInput) {
-        nextInput.focus();
-      }
-    }
-  });
-});
-DOMSelectors.rows.forEach((input) => console.log(input));
+// DOMSelectors.rows.forEach((tiles, index) => {
+//   DOMSelectors.tiles.addEventListener("input", () => {
+//     if (tiles.value.length === tiles.maxLength) {
+//       const nextInput = tiles[index + 1];
+//       if (nextInput) {
+//         nextInput.focus();
+//       }
+//     }
+//   });
+// });
